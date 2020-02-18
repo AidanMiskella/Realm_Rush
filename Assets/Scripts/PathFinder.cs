4affe78b38e20cc2aafa1730c2c8c6eb0dcca95d
Aidan Miskella <aidanmiskella10@gmail.com>
2020-02-18 12:12:45 +0000

    colour waypoint

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 
 public class PathFinder : MonoBehaviour {
 
+    [SerializeField] Waypoint startWaypoint, endWaypoint;
+
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
 
     // Start is called before the first frame update
     void Start() {
 
         LoadBlocks();
+        ColorStartAndEnd();
+    }
+
+    private void ColorStartAndEnd() {
+
+        startWaypoint.SetTopColor(Color.blue);
+        endWaypoint.SetTopColor(Color.red);
     }
 
     private void LoadBlocks() {
@@ -27,7 +36,6 @@ public class PathFinder : MonoBehaviour {
                 grid.Add(gridPos, waypoint);
             }
         }
-        print("Loaded " + grid.Count + "blocks");
     }
 
     // Update is called once per frame
