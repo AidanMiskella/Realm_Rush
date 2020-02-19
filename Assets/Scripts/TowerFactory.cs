0dffea31879ad299c560d79d4ff360a904737584
Aidan Miskella <aidanmiskella10@gmail.com>
2020-02-19 11:30:43 +0000

    max towers part 2

--- Assets/Scripts/TowerFactory.cs
+++ Assets/Scripts/TowerFactory.cs
@@ -5,30 +5,38 @@ using UnityEngine;
 public class TowerFactory : MonoBehaviour {
 
     [SerializeField] Tower towerPrefab;
-    [SerializeField] int maxTowers = 5;
+    [SerializeField] int towerLimit = 5;
 
-    int numTowers = 0;
+    Queue<Tower> towerQueue = new Queue<Tower>();
 
     public void AddTower(Waypoint baseWaypoint) {
 
-        if (numTowers < maxTowers) {
+        int numTowers = towerQueue.Count;
+
+        if (numTowers < towerLimit) {
 
             InstantiateNewTower(baseWaypoint);
         } else {
 
-            MoveExistingTower();
+            MoveExistingTower(baseWaypoint);
         }
     }
 
     private void InstantiateNewTower(Waypoint baseWaypoint) {
 
-        Instantiate(towerPrefab, baseWaypoint.transform.position, Quaternion.identity);
+        var newTower = Instantiate(towerPrefab, baseWaypoint.transform.position, Quaternion.identity);
         baseWaypoint.isPlaceable = false;
-        numTowers++;
+
+        // set the basewaypoint
+        // put new tower on queue
+        towerQueue.Enqueue(newTower);
     }
 
-    private static void MoveExistingTower() {
+    private void MoveExistingTower(Waypoint baseWaypoint) {
 
-        Debug.Log("Max towers reached");
+        var oldTower = towerQueue.Dequeue();
+        baseWaypoint.isPlaceable = true;
+        // set basewaypoint
+        towerQueue.Enqueue(oldTower);
     }
 }
